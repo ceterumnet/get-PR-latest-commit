f5bee45879a68f5029f38eba4965d09b54c4da41
Dave Raphael <dave@singularity.consulting>
2021-01-05 12:06:53 -0600

    feat: initial capability for v1.0

--- index.test.js
+++ index.test.js
@@ -3,21 +3,13 @@ const process = require('process');
 const cp = require('child_process');
 const path = require('path');
 
-test('throws invalid number', async () => {
-  await expect(wait('foo')).rejects.toThrow('milliseconds not a number');
-});
-
-test('wait 500 ms', async () => {
-  const start = new Date();
-  await wait(500);
-  const end = new Date();
-  var delta = Math.abs(end - start);
-  expect(delta).toBeGreaterThanOrEqual(500);
+test('throws empty repository', () => {
+  expect(wait('foo')).rejects.toThrow('milliseconds not a number');
 });
 
 // shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = 500;
+  process.env['INPUT_REPOSITORY'] = '/ceterumnet/dummy-repo';
   const ip = path.join(__dirname, 'index.js');
   console.log(cp.execSync(`node ${ip}`, {env: process.env}).toString());
-})
+});
